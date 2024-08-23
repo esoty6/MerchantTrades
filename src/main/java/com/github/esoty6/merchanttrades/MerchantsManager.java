c3e3c3b6c31f57dcfafa6e4a158263f969fa8c80
esoty6 <kamilwitek12@o2.pl>
2024-08-23 18:52:18 +0200

    Create merchants manager class

---
+++ src/main/java/com/github/esoty6/merchanttrades/MerchantsManager.java
@@ -0,0 +1,24 @@
+package com.github.esoty6.merchanttrades;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.Villager.Profession;
+import org.bukkit.event.entity.VillagerAcquireTradeEvent;
+import com.github.esoty6.merchanttrades.merchants.Cleric;
+import com.github.esoty6.merchanttrades.merchants.CustomMerchant;
+
+public class MerchantsManager {
+  private Map<Villager, CustomMerchant> merchantsMap = new HashMap<>();
+
+  protected void createMerchant(Villager villager, VillagerAcquireTradeEvent event) {
+    Profession villagerProfession = villager.getProfession();
+
+    if (villagerProfession.equals(Profession.CLERIC)) {
+      merchantsMap.putIfAbsent(villager, new Cleric(villager));
+      merchantsMap.get(villager).createCustomRecipe(event.getRecipe());
+
+      event.setCancelled(true);
+    }
+  }
+}
